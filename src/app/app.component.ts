f39e5637b4327557b146f43ae4555ef7249c2ee0
Chad Sheets <cjsheets@gmail.com>
2017-01-02 20:03:23 -0800

    updated providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,22 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AuthService } from './auth/auth.service';
 import { Logger } from './shared/logger.service';
 
 import { Subject } from 'rxjs/Subject';
-import {FirebaseAuth} from 'angularfire2';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app works!';
-
-  private loggedIn = new Subject<boolean>();
 
   constructor(
     private _auth: AuthService,
-    private _log: Logger,
-    @Inject(FirebaseAuth) public auth: FirebaseAuth
+    private _log: Logger
   ) {}
 
-  // login() {
-  //   this.af.auth.login();
-  // }
-
-  // logout() {
-  //    this.af.auth.logout();
-  // }
-
-  ngOnInit(): void {
-    this._auth.isLoggedIn().subscribe(loggedIn => this.loggedIn = loggedIn);
-  }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, AuthProviders } from 'angularfire2';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -8,7 +8,6 @@ import { Logger } from '../shared/logger.service';
 
 @Injectable()
 export class AuthService {
-  private loggedIn = new Subject<boolean>();
 
   // store the URL so we can redirect after logging in
   redirectUrl: string;
@@ -20,24 +19,26 @@ export class AuthService {
     this.af.auth.subscribe((auth) => this._log['log']( "AuthService: " + auth ));
   }
 
-  login(provider = '{}'): void {
-        this.af.auth.login(provider).then((success) => {
-       this.loggedIn.next( true );
+  login(provider = ''): void {
+    var authProvider = {};
+    switch (provider){
+      case 'Google': authProvider = {provider: AuthProviders.Google}; break;
+      case 'Github': authProvider = {provider: AuthProviders.Github}; break;
+      case 'Facebook': authProvider = {provider: AuthProviders.Facebook}; break;
+      case 'Twitter': authProvider = {provider: AuthProviders.Twitter}; break;
+    }
+    this._log['log']( "auth.service: login() - " + authProvider );
+    this.af.auth.login(authProvider).then((success) => {
+         
     }).catch((err) => {
-      this.loggedIn.next( false );
+
       this._log['log']( "Error Logging In:" )
       this._log['warn']( err )
     });
   }
 
   logout(): void {
-    this.loggedIn.next( false );
     this.af.auth.logout();
   }
-
-    isLoggedIn(): Observable<any> {
-      // return observable to be notified of status updates (login/logout)
-      return this.loggedIn.asObservable();
-    }
 
 }
